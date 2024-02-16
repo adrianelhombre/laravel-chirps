0e20ec8bf2aff13eaf30ffcb6a5619a54664de19
adrianelhombre <adrianelhombrehernandez@gmail.com>
2024-02-16 13:28:42 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,6 @@ export default defineConfig({
         }),
     ],
     build: {
-        outDir: 'public/dist', // Cambia 'public/dist' por la ruta deseada
+        outDir: 'dist', // Cambia 'public/dist' por la ruta deseada
     },
 });
